b56b40e737935b1dc54d9d6ff792367a5c9ba824
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-11-09 15:45:59 +0100

    Bonus: task completed

---
+++ Bonus/js/script.js
@@ -0,0 +1,27 @@
+Vue.config.devtools = true;
+
+new Vue({
+    el: "#root",
+    data: {
+        apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
+        newMail: "",
+        mailList: [],
+        totalEmails: 0,
+    },
+    methods: {
+        generateMultipleMails(){
+            
+            for (let i = 0; i < 10; i++) {
+                
+                axios.get(this.apiUrl).then((responseServer)=>{
+                    this.totalEmails++
+                    let email =  responseServer.data.response;
+                    this.mailList.push(email)
+                })
+            }
+
+
+        } 
+    },
+
+})
